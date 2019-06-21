31a731034f80239f40f41116f5815c79ef274bbc
Mahdi amolimoghadam <49509350+915030705@users.noreply.github.com>
2019-06-21 21:36:12 +0430

    TWFeXuv1Ect1UW22b36MT3fE7hKvdXBBih
    https://office.com/gamma.mahdii@gmail.com

--- Mainnet-token-contracts-20180610/contracts/0x09debe702678140c1be278213109719fab98d0d8-MOL-MolikToken.sol
+++ Mainnet-token-contracts-20180610/contracts/0x09debe702678140c1be278213109719fab98d0d8-MOL-MolikToken.sol
@@ -130,6 +130,6 @@ contract MolikToken is StandardToken {
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
         //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
         if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
+        return true;https://office.com/gamma.mahdii@gmail.com 
     }
 }
